d3c757501e46db57edd2a62ffb1446c3cc4d008f
hooy21c@gmail.com <hooy21c@gmail.com>
2020-01-02 17:28:28 +0900

    Updates

---
+++ precache-manifest.fac266888f9df17669d6ced9bd060fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d94ac3b26b92b05da76be5996552e77",
+    "url": "/movie-app-2020-2/index.html"
+  },
+  {
+    "revision": "c5374f5b7bb2f8b4eab6",
+    "url": "/movie-app-2020-2/static/css/main.eeba9af8.chunk.css"
+  },
+  {
+    "revision": "431bc7a56fc4d0ce9842",
+    "url": "/movie-app-2020-2/static/js/2.8ae3678a.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie-app-2020-2/static/js/2.8ae3678a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5374f5b7bb2f8b4eab6",
+    "url": "/movie-app-2020-2/static/js/main.6d9a61ee.chunk.js"
+  },
+  {
+    "revision": "2a326ce9a1335ba3b6ba",
+    "url": "/movie-app-2020-2/static/js/runtime-main.5202d687.js"
+  }
+]);
